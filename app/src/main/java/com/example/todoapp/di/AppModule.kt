69b635f05b9048a2051c2be5c66394bbe193fd16
tadotcom <kh.sk.ti.1820@gmail.com>
2025-12-30 02:25:38 +0900

    UseCaseには@InjectをつけずModuleで生成する

---
+++ app/src/main/java/com/example/todoapp/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp.di
+
+import com.example.todoapp.data.repository.TodoRepositoryImpl
+import com.example.todoapp.domain.repository.TodoRepository
+import com.example.todoapp.domain.usecase.GetTodosUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(): TodoRepository {
+        return TodoRepositoryImpl()
+    }
+    
+    @Provides
+    @Singleton
+    fun provideGetTodosUseCase(repository: TodoRepository): GetTodosUseCase {
+        return GetTodosUseCase(repository)
+    }
+}
